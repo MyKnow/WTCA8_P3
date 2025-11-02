dbdd6b7d99e3863d2bfcaca4dff1a37add4fa874
MyKnow <myknow000@gmail.com>
2025-11-02 13:05:30 +0900

    refactor(LottoRank): hasBonus 프로퍼티명을 bonusRequired로 변경하여 의미 명확화

--- src/main/kotlin/lotto/constant/LottoConstant.kt
+++ src/main/kotlin/lotto/constant/LottoConstant.kt
@@ -10,12 +10,12 @@ enum class LottoRule(val value: Int) {
 enum class LottoRank(
     val matchCount: Int,
     val reward: Int,
-    val hasBonus: Boolean = false,
+    val bonusRequired: Boolean = false,
 ) {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000, hasBonus = true),
+    SECOND(5, 30_000_000, bonusRequired = true),
     THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
     FIFTH(3, 5_000),
-    NONE(0, 0),
+    NONE(0, 0);
 }
